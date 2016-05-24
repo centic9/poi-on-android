c01733fb2e892debd2310a8a9bc4983e92e0cc11
Dominik Stadler <dominik.stadler@gmx.at>
2016-05-24 22:07:51 +0200

    Use alto-xml from fasterxml and set system properties so the correct implementation is loaded, this finally seems to work now

--- poitest/src/main/java/org/dstadler/poiandroidtest/poitest/DocumentListActivity.java
+++ poitest/src/main/java/org/dstadler/poiandroidtest/poitest/DocumentListActivity.java
@@ -41,6 +41,10 @@ public class DocumentListActivity extends Activity
         setContentView(R.layout.activity_document_list);
 
 
+        System.setProperty("org.apache.poi.javax.xml.stream.XMLInputFactory", "com.fasterxml.aalto.stax.InputFactoryImpl");
+        System.setProperty("org.apache.poi.javax.xml.stream.XMLOutputFactory", "com.fasterxml.aalto.stax.OutputFactoryImpl");
+        System.setProperty("org.apache.poi.javax.xml.stream.XMLEventFactory", "com.fasterxml.aalto.stax.EventFactoryImpl");
+
         try {
             Workbook wb = new XSSFWorkbook();
             Sheet sheet = wb.createSheet("Sheet1");
