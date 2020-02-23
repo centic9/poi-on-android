4f10e6c2c94673a904615f0177df4b2fc743fd65
Dominik Stadler <dominik.stadler@gmx.at>
2020-02-23 09:58:55 +0100

    Remove dependency on JUnit in the poitest Android App as compiling fails with error messages otherwise

--- poitest/src/main/java/org/apache/poi/poifs/crypt/TestSignatureInfo.java
+++ poitest/src/main/java/org/apache/poi/poifs/crypt/TestSignatureInfo.java
@@ -1,15 +1,13 @@
 package org.apache.poi.poifs.crypt;
 
 import org.apache.poi.poifs.crypt.dsig.SignatureInfo;
-import org.junit.Test;
-
-import static org.junit.Assert.assertNotNull;
 
 public class TestSignatureInfo {
-    @Test
-    public void testConstruct() throws Exception {
-        // simple test which just verifies that we can load the class and all dependend classes
+    public void testConstruct() {
+        // simple test which just verifies that we can load the class and all dependent classes
         SignatureInfo info = new SignatureInfo();
-        assertNotNull(info);
+        if(info.toString() == null) {
+            throw new IllegalStateException("ToString is null");
+        }
     }
 }
