e94ee3c2e6706d9cf20492468b7318733fc80a28
Dominik Stadler <dominik.stadler@gmx.at>
2020-06-01 21:18:38 +0200

    Convert test to JUnit 4

--- poitest/src/main/java/org/apache/poi/poifs/crypt/TestSignatureInfo.java
+++ poitest/src/main/java/org/apache/poi/poifs/crypt/TestSignatureInfo.java
@@ -1,13 +1,15 @@
 package org.apache.poi.poifs.crypt;
 
 import org.apache.poi.poifs.crypt.dsig.SignatureInfo;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertNotNull;
 
 public class TestSignatureInfo {
-    public void testConstruct() {
-        // simple test which just verifies that we can load the class and all dependent classes
+    @Test
+    public void testConstruct()  {
+        // simple test which just verifies that we can load the class and all dependend classes
         SignatureInfo info = new SignatureInfo();
-        if(info.toString() == null) {
-            throw new IllegalStateException("ToString is null");
-        }
+        assertNotNull(info);
     }
 }
