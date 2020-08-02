23cf2e7da4f237a51a9847ceca5c5376144c6ff4
Dominik Stadler <dominik.stadler@gmx.at>
2020-08-02 13:39:26 +0200

    Update to newer SDK release 28

--- poitest/src/androidTest/java/org/dstadler/poiandroidtest/poitest/ApplicationTest.java
+++ poitest/src/androidTest/java/org/dstadler/poiandroidtest/poitest/ApplicationTest.java
@@ -1,13 +1,19 @@
 package org.dstadler.poiandroidtest.poitest;
 
-import android.app.Application;
-import android.test.ApplicationTestCase;
+import org.dstadler.poiandroidtest.poitest.dummy.DummyContent;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-    public ApplicationTest() {
-        super(Application.class);
+@RunWith(AndroidJUnit4.class)
+public class ApplicationTest {
+    @Test
+    public void test() {
+        // just some very simple things to verify that testing works basically
+        DummyContent.addItem(new DummyContent.DummyItem("1", "bla", "some text"));
     }
 }
--- poitest/src/main/java/org/apache/poi/poifs/crypt/TestSignatureInfo.java
+++ poitest/src/main/java/org/apache/poi/poifs/crypt/TestSignatureInfo.java
@@ -1,12 +1,10 @@
 package org.apache.poi.poifs.crypt;
 
 import org.apache.poi.poifs.crypt.dsig.SignatureInfo;
-import org.junit.Test;
 
 import static junit.framework.Assert.assertNotNull;
 
 public class TestSignatureInfo {
-    @Test
     public void testConstruct()  {
         // simple test which just verifies that we can load the class and all dependend classes
         SignatureInfo info = new SignatureInfo();
