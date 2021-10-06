6d4775f6ec4fbfba042a49b0475cdeb890664cad
Dominik Stadler <dominik.stadler@gmx.at>
2021-10-06 17:55:27 +0200

    Update comment on item that crashes on purpose currently

--- poitest/src/main/java/org/apache/poi/poifs/crypt/TestSignatureInfo.java
+++ poitest/src/main/java/org/apache/poi/poifs/crypt/TestSignatureInfo.java
@@ -6,7 +6,8 @@ import static junit.framework.Assert.assertNotNull;
 
 public class TestSignatureInfo {
     public void testConstruct()  {
-        // simple test which just verifies that we can load the class and all dependend classes
+        // simple test which just verifies that we can load the class and all dependent classes
+        // NOTE: this will currently crash because of missing classes
         SignatureInfo info = new SignatureInfo();
         assertNotNull(info);
     }
