e7817914e4f8f1f257aac5c0d34561404cad245f
Dominik Stadler <dominik.stadler@gmx.at>
2023-12-26 21:10:18 +0100

    Minimize application further

--- poitest/src/main/java/org/dstadler/poiandroidtest/poitest/MainActivity.java
+++ poitest/src/main/java/org/dstadler/poiandroidtest/poitest/MainActivity.java
@@ -3,10 +3,17 @@ package org.dstadler.poiandroidtest.poitest;
 import android.app.Activity;
 import android.os.Bundle;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class MainActivity extends Activity {
+	private static final Logger LOG = LogManager.getLogger(MainActivity.class);
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+
+		LOG.info("Testing...");
 	}
 }
